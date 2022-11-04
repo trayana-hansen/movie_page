02cf5a6c7e4abb412cf9fdae8922f9c5b07219a4
Philip Thomsen <philip-th@live.dk>
2022-11-04 10:48:03 +0100

    Reset

--- assets/js/footer.js
+++
@@ -1,9 +0,0 @@
-const date = new Date();
-let year = date.getFullYear();
-console.log(year);
-
-document.getElementById('year').innerHTML += `
-
-<p>Copyright ${year} </p>
-
-`
--- assets/js/loader.js
+++
@@ -1,9 +0,0 @@
-function loading() {
-	setTimeout(() => {
-		document.getElementById("first").click()
-	}, 3000);
-}
-loading()
-
-
-
